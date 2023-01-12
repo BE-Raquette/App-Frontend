6864bce4e2a48d54dd1b29c52c44cd07c4574545
Elise REY <elise.rey@isen.yncrea.fr>
2023-01-12 12:47:49 +0100

    Splash screen added

---
+++ app/src/main/java/fr/isen/racketselectorapp/SplashScreen.kt
@@ -0,0 +1,32 @@
+package fr.isen.racketselectorapp
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import fr.isen.racketselectorapp.databinding.SplashScreenBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+    lateinit var binding: SplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = SplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
